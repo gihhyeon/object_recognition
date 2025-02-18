28cb9d1fce1fd96a1270194dd7194ebe2d6a58c5
gihhyeon <fineblue.kh@gmail.com>
2025-02-18 14:37:38 +0900

    003_MySQL 연동

---
+++ src/main/java/com/knu/object_recognition/entity/Video.java
@@ -0,0 +1,57 @@
+package com.knu.object_recognition.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "upload_date")
+    private Instant uploadDate;
+
+    @Column(name = "file_url", nullable = false, length = 500)
+    private String fileUrl;
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public Instant getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(Instant uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
